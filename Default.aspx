dc583cfd19f87331cc3f880c2aff2bfce3bbfb28
Luka Petranović <luka.petranovic@gmail.com>
2021-11-17 23:03:31 +0100

    Resolved: - TextBox that allows user to enter hard coded magic word - Button to check if magic word is actually magic word - Label to explain what user should enter - TODO list for future task

--- Default.aspx
+++ Default.aspx
@@ -4,6 +4,14 @@
 </asp:Content>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-    Vježba 1:
+    Vježba 1:<br />
         Hello, World!
+    <hr />
+    Vježba 2:<br />
+    <asp:Label ID="Label1" runat="server" Text="Unesi magičnu riječ:"></asp:Label>
+    <asp:Label ID="Label2" runat="server" Text="Magična riječ pogođena:" Visible="False"></asp:Label>
+    <asp:Label ID="Label3" runat="server" Text="Magična riječ nije pogođena:" Visible="False"></asp:Label>
+    <asp:TextBox ID="EnterMagicWord_Text" runat="server"></asp:TextBox>
+    <asp:Button ID="CheckMagicWord_Button" runat="server" Text="Provjeri" OnClick="CheckMagicWord_Button_Click" />
+
 </asp:Content>
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -9,9 +9,29 @@ namespace PIN_Labaratoriji
 {
     public partial class _Default : Page
     {
+        private string MagicWord = "magija";
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (IsPostBack)
+            {
+                checkMagicWord();
+            }
+        }
+        private void checkMagicWord()
+        {
+            // kod za provjeru
+            if (EnterMagicWord_Text.Text == MagicWord)
+            {
+                Label1.Visible = false;
+                Label2.Visible = true;
+            }
+            {/*TODO 01: Add Label3 for word that is not magic word
+               TODO 02: Add if else function that will switch labels from right/ wrong magic word entered*/}
+        }
 
+        protected void CheckMagicWord_Button_Click(object sender, EventArgs e)
+        {
+            checkMagicWord();
         }
     }
 }
